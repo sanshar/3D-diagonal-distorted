bf5701b824492dc5504ca6921f8ba99dc1a174e7
Sandeep Sharma <sanshar@gmail.com>
2024-05-21 07:10:18 -0600

    added numerical 1-D fit

--- Fit1dDeformation.py
+++ Fit1dDeformation.py
@@ -58,9 +58,7 @@ def rho_erf(params, x, Zi, pos, a1, b1):
 
     d = (jsp.special.erf(R * Zi / a) - jsp.special.erf( R / b))/R 
 
-    integral = integralRho_Erf(params, b1, Zi, pos) - integralRho_Erf(params, a1, Zi, pos)
-
-    return (jnp.einsum('ij->i', d) + shift)/integral, params/integral
+    return (jnp.einsum('ij->i', d) + shift)
 
 ##single atom
 @jit
@@ -72,12 +70,10 @@ def rho_Gauss(params, x, Zi, pos, a1, b1):
     fval = params[:N] * jnp.exp(-params[N:] * R**2) 
 
     integral = integralRho_Gauss(params, b1*jnp.ones((1,)), Zi, pos) - integralRho_Gauss(params, a1*jnp.ones((1,)), Zi, pos)
-    normalizedParams = 1.*params
-    normalizedParams.at[:N].set(params[:N]/integral)
-    return jnp.einsum('ij->i', fval)/integral, normalizedParams
+    return jnp.einsum('ij->i', fval)/integral
 
 @jit 
-def Tmap(params, x, Zi, center, a1, b1):
+def Tmap_Gauss(params, x, Zi, center, a1, b1):
     L1 = b1 - a1
     T0 = integralRho_Gauss(params, a1*jnp.ones((1,)), Zi*jnp.ones((1,)), center*jnp.ones((1,)))
     T1 = integralRho_Gauss(params, b1*jnp.ones((1,)), Zi*jnp.ones((1,)), center*jnp.ones((1,)))
@@ -86,7 +82,7 @@ def Tmap(params, x, Zi, center, a1, b1):
     
 
 @jit
-def invMap(params, y, Zi, center, a1, b1):
+def invMap_Gauss(params, y, Zi, center, a1, b1):
 
     tol, maxIter, alpha = 1.e-7, 6000, 0.05
 
@@ -100,7 +96,7 @@ def invMap(params, y, Zi, center, a1, b1):
     def body_fun(carry):
         x, T, iter = carry
         x = x - alpha * (T - y)
-        T = Tmap(params, x, Zi, center, a1, b1)
+        T = Tmap_Gauss(params, x, Zi, center, a1, b1)
         return (x, T, iter+1)
 
     @jit
@@ -109,7 +105,7 @@ def invMap(params, y, Zi, center, a1, b1):
         return carry, r
 
     x = y
-    init_val = (x, Tmap(params, x, Zi, center, a1, b1), 0)
+    init_val = (x, Tmap_Gauss(params, x, Zi, center, a1, b1), 0)
     carry, r = jax.lax.scan(body_scan, init_val, None, maxIter)
     #carry = lax.while_loop(cond_fun, body_fun, init_val)
     return carry[0], carry[2]
@@ -136,28 +132,22 @@ def gaussVal1D(alpha, m, center, x, A):
     #val += (center-X-A)**m*jnp.exp(-alpha*(center-X-A)**2)
     return val
 
-@jit
+#@jit
 def getError(params, alpha, m, Zi, center, x, a1, b1, Sref):
     L1 = b1-a1
 
     ##get deformed grid
-    S, _ = invMap(params, x, Zi, center, a1, b1)
-    Jac, _ = rho_Gauss(params, S, Zi*jnp.ones((1,)), center*jnp.ones((1,)), a1, b1) 
-    Jac = (Jac * L1)
-
+    S, _ = invflow_numeric(x, a1, b1, params)
+    x2, Jac = flow_numeric(S, a1, b1, params) 
+    print(jnp.max(x-x2))
 
     aovals = gaussVal1D(alpha, m, center, S, L1)
     S = jnp.einsum('ai,a,aj->ij', aovals, 1./Jac, aovals) * (x[1]-x[0])
 
-    return jnp.linalg.norm(S-Sref)
+    return jnp.linalg.norm(S-Sref), S
 
-def fitDeformation(mf, basMesh):
-    A = mf.cell.lattice_vectors()
-    a1,b1 = 0.,A[0,0]
-    L1 = b1-a1
 
-    denseGrid = 1000
-
+def makeBasisInformation(mf):
     l = mf.cell._bas[:,1]
     nao = np.sum(l+1)
     alpha, m = np.zeros((nao,)), np.zeros((nao,))
@@ -172,18 +162,118 @@ def fitDeformation(mf, basMesh):
     Zi = mf.cell._atm[0,0]
 
     alpha, m = jnp.asarray(alpha), jnp.asarray(m)
+    return alpha, m, Zi, center
+
+def gfun(x,a,b,nb):
+  nx = x.shape[0]
+  u = jnp.reshape( (x-a)/(b-a), (nx,1) )
+  n = jnp.reshape( jnp.arange(nb), (1,nb) ) - nb//2
+  g = jnp.exp(-2*jnp.pi*1j*n*u) /(b-a)**0.5
+  return g
+
+def Gfun(x,a,b,nb):
+
+  nx = x.shape[0]
+  u = jnp.reshape( (x-a)/(b-a), (nx,1) )
+
+  nL = jnp.arange(0,nb//2) - nb//2
+  nR = jnp.arange(nb//2 + 1, nb ) - nb//2
+
+  GL = (jnp.exp(-2*jnp.pi*1j*nL*u) - 1)/(-2*jnp.pi*1j*nL)
+  G0 = u
+  GR = (jnp.exp(-2*jnp.pi*1j*nR*u) - 1)/(-2*jnp.pi*1j*nR)
+
+  G = (b-a) * jnp.hstack((GL,G0,GR)) / (b-a)**0.5
+  return G
+
+
+def fitFourier(F, g1):
+    c = jnp.einsum('i,ia->a', F, g1)
+    return c
+
+@jit
+def flow_numeric(x, a1, b1, c):
+    L1 = b1-a1
+
+    nb1 = c.shape[0]
+    g1 = gfun(x,a1,b1,nb1)
+    G1 = Gfun(x,a1,b1,nb1)
+    rho = jnp.einsum('a,ia->i',c,g1)
+
+    T1 = a1 + L1 * jnp.einsum('a,ia->i', c, G1)
+
+    return T1.real, rho.real*L1
+
+@jit
+def invflow_numeric(y, a1, b1, c):
+
+    tol, maxIter, alpha = 1.e-7, 10000, 0.01
+
+    @jit
+    def cond_fun(carry):
+        x, T, iter = carry
+        err = jnp.max(jnp.abs(x-T))/(b1-a1)
+        return (err > tol) * (iter < maxIter)
+
+    @jit 
+    def body_fun(carry):
+        x, T, iter = carry
+        x = x - alpha * (T - y)
+        T, _ = flow_numeric(x, a1, b1, c)
+        return (x, T.real, iter+1)
+
+    @jit
+    def body_scan(carry, r):
+        carry = body_fun(carry)
+        return carry, r
+
+    x = y
+    init_val = (x, flow_numeric(x, a1, b1, c)[0].real, 0)
+    #carry, r = jax.lax.scan(body_scan, init_val, None, maxIter)
+    carry = lax.while_loop(cond_fun, body_fun, init_val)
+    return carry[0].real, carry[2]
+
+##fit the deformation
+def fitDeformation(mf, basMesh):
+    A = mf.cell.lattice_vectors()
+    a1,b1 = 0.,A[0,0]
+    L1 = b1-a1
+
+    denseGrid = 1000
+
+    ##make the reference overlap matrix
+    alpha, m, Zi, center = makeBasisInformation(mf)
     denseX = jnp.arange(a1, b1, L1/denseGrid)
     vals = gaussVal1D(alpha, m, center, denseX, L1)
-
     Sref = vals.T.dot(vals) * L1/denseGrid
 
-    #params = jnp.asarray([0.1, 2., 0.05])
-    params = jnp.asarray([2.3099184,   6.475843,   12.867022, 0.05, 1.6527886,  26.1984,    180.28716,0.] )
 
+    ##initital guess for parameters
+    params = jnp.asarray([0.1, 2., 0.05])
+    #params = jnp.asarray([2.3099184,   6.475843,   12.867022, 0.05, 1.6527886,  26.1984,    180.28716,0.] )
+
+    for a in [0.1,0.2,0.3]:
+        for b in [2.0, 3.0, 4.]:
+            for c in [0.2, 0.1, 0.05]:
+
+                params = jnp.asarray([a,b,c])
+                g1 = gfun(denseX, a1, b1, denseGrid)
+                G1 = Gfun(denseX, a1, b1, denseGrid)
+                m1 = jnp.ravel( Gfun(b1*jnp.ones(1), a1, b1, denseGrid))
+                rhoVal = rho_erf(params, denseX, Zi*jnp.ones((1,)), center*jnp.ones((1,)), a1, b1)
+                C = fitFourier(rhoVal, g1) * L1/denseGrid
+                Norm = jnp.einsum('a,a', C, m1).real
+                C = C/Norm
+
+                x = jnp.arange(a1, b1, L1/basMesh)
+                error, S =  getError(C, alpha, m, Zi, center, x, a1, b1, Sref)
+                print("{0:8.2f} {1:8.2f}  {2:8.2f}  {3:10.2f}".format(a,b,c,error))
+
+    '''
     x = jnp.arange(a1, b1, L1/basMesh)
     #Jac, normalizedParams = rho_Gauss(params, x, Zi*jnp.ones((1,)), center*jnp.ones((1,)), a1, b1)
-    T = Tmap(params,  x, Zi, center, a1, b1)
-    S, iter = invMap(params, T, Zi, center, a1, b1)
+    T = Tmap_Gauss(params,  x, Zi, center, a1, b1)
+    S, iter = invMap_Gauss(params, T, Zi, center, a1, b1)
 
     print("inversion error ", iter, abs(S-x).max())
     import pdb
@@ -205,8 +295,8 @@ def fitDeformation(mf, basMesh):
     pdb.set_trace()
 
     Jac, normalizedParams = rho_Gauss(params, denseX, Zi*jnp.ones((1,)), center*jnp.ones((1,)), a1, b1)
-    T = Tmap(params,  denseX, Zi, center, a1, b1)
-    S, iter = invMap(params, denseX, Zi, center, a1, b1)
+    T = Tmap_Gauss(params,  denseX, Zi, center, a1, b1)
+    S, iter = invMap_Gauss(params, denseX, Zi, center, a1, b1)
 
     print("inversion error ", iter)
     plt.plot(denseX, Jac)
@@ -217,7 +307,7 @@ def fitDeformation(mf, basMesh):
     import pdb
     pdb.set_trace()
     print(jnp.sum(Jac)*L1/denseGrid)
-
+    '''
     #getError(params, alpha, m, Zi, center, a1, b1, Sref, basMesh[0])
 
 if __name__ == "__main__":
@@ -231,7 +321,7 @@ if __name__ == "__main__":
     print(hyp22((-10)))
     exit(0)
     '''
-    nG = 10
+    nG = 20
     cell = pgto.M(
         a = 8*np.eye(3),
         atom = '''Be 4. 4. 4.''',
